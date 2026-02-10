69b5b2c090744973f25f70840c370b1edc16b09f
BoBeR24 <denver2004.dg@gmail.com>
2026-02-10 16:42:05 +0100

    Made code for compromise shorted and easier to read. Implemented burying strategy. Changed strat_voting_all to print only the best strategy, instead of all improved strategies

--- testing.py
+++ testing.py
@@ -1,10 +1,10 @@
 from happiness import BasicHappiness
-from strategic_voting import compromise, strat_voting_all
+from strategic_voting import compromise, burying, strat_voting_all
 from voting import VotingSystem
 import numpy as np
 
 def main():
-    schema = [1, 1, 0, 0]
+    schema = [1, 1, 1, 0]
     true_preferences = [["A", "C", "B", "D"], ["B", "D", "A", "C"], ["B", "D", "A", "C"], ["C", "D", "B", "A"],
                         ["D", "C", "A", "B"]]
 
@@ -23,9 +23,15 @@ def main():
     strategic_vote, max_happiness, new_situation = compromise(VS, 1)
     print("strategic vote result(compromise):", strategic_vote, max_happiness, new_situation)
 
-    print("situation with strategic voting:", VS.vote(new_situation))
+    print("situation with compromise voting:", VS.vote(new_situation))
 
-    # strat_voting_all(schema, true_preferences, hap, hap_metric)
+    strategic_vote, max_happiness, new_situation = burying(VS, 1)
+    print("strategic vote result(burying):", strategic_vote, max_happiness, new_situation)
+
+    print("situation with burying voting:", VS.vote(new_situation))
+
+    print("Optimal strategic vote(independent from specific strategy types):\n")
+    strat_voting_all(VS, hap, hap_metric)
 
 
 if __name__ == "__main__":
